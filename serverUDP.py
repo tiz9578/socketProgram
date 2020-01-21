9fae8e7cad044f72dd41b1838cb9a24e68ba14bf
tiz9578 <tiziano.minuzzo@gmail.com>
2020-01-21 09:45:27 +0100

    New version UDP srv
    update : transfer, connect/accept/bye message, error handling

--- serverUDP.py
+++ serverUDP.py
@@ -3,17 +3,13 @@ Created on Jan 17, 2020
 
 @author: tiz
 '''
-'''
-Created on Jan 17, 2020
-
-@author: tiz
-'''
 from socket import *
 from requests import *
 import requests
 import os
 import sys
 from timeit import default_timer as timer
+import time
 
 serverPort = 4000
 serverSocket = socket(AF_INET,SOCK_DGRAM)
@@ -21,9 +17,19 @@ serverSocket = socket(AF_INET,SOCK_DGRAM)
 serverSocket.bind(('',serverPort))
 
 #serverSocket.listen(1)
-
 print 'The server is ready to receive'
 while 1:
+
+    connect_from_client, clientAddress = serverSocket.recvfrom(2048)
+    print 'From client: '+ connect_from_client
+
+    accept_connection = 'accept'
+    serverSocket.sendto(accept_connection, clientAddress)
+
+
+
+
+
     fileurl, clientAddress = serverSocket.recvfrom(2048)
     #connectionSocket, addr = serverSocket.accept()
     #fileurl = connectionSocket.recv(4000)
@@ -46,6 +52,10 @@ while 1:
         print ("Timeout Error:",errTime)
         serverSocket.sendto('Timeout Error', clientAddress)
         sys.exit(1)
+    #except:
+     #   print ("Generic Error")
+      #  serverSocket.sendto('Generic Error', clientAddress)
+       # sys.exit(1)
     
     
     # Check the file size
@@ -57,23 +67,37 @@ while 1:
     # Find the name of the file based on URL
     if fileurl.find('/'):
         print 'Getting ' + fileurl.rsplit('/', 1)[1]
+       # fileToClient = fileurl.rsplit('/', 1)[1]
         fileToClient = fileurl.rsplit('/', 1)[1]
     
-    
     # Create an empty file to store the content of the file
+    #######OKOK open(fileToClient, 'wb').write(r.content)
     open(fileToClient, 'wb').write(r.content)
+    
     
     
     #First: send the size of the file from the header to the client to compare
 
     #connectionSocket.send(fileSize)
-    serverSocket.sendto(fileSize, clientAddress)
-    
+    message = "clientFile"+fileToClient
+    serverSocket.sendto(message, clientAddress)
+    #serverSocket.sendto(fileToClient, clientAddress)
     
     #Second send the file to client
     
     #connectionSocket.send(fileToClient)
     serverSocket.sendto(fileToClient, clientAddress)
+    
+    f=open(fileToClient,"rb")
+    dataTosend = f.read(1024)
+    while (dataTosend):
+        if(serverSocket.sendto(dataTosend,clientAddress)):
+            #print "sending ..."
+            dataTosend = f.read(1024)
+            time.sleep(0.02)# Give receiver a bit time to save
+    f.close()
+    
+    #Send the size:
     
     
     
@@ -92,5 +116,5 @@ while 1:
         print
         #connectionSocket.send(str(timeTodw))
         serverSocket.sendto(str(totTime), clientAddress)
-        #connectionSocket.close()
+        #serverSocket.close()
 
