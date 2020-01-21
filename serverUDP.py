263819892ddc1426cf89a2f1ee8ec868f34ba1fe
tiz9578 <tiziano.minuzzo@gmail.com>
2020-01-21 18:28:05 +0100

    Update serverUDP.py

--- serverUDP.py
+++ serverUDP.py
@@ -1,7 +1,7 @@
 '''
 Created on Jan 17, 2020
 
-@author: tiz
+@author: tiz - UDP Server
 '''
 from socket import *
 from requests import *
@@ -16,9 +16,11 @@ serverSocket = socket(AF_INET,SOCK_DGRAM)
 
 serverSocket.bind(('',serverPort))
 
-#serverSocket.listen(1)
+#server in listening mode
 print 'The server is ready to receive'
 while 1:
+
+    # Connect - accept messages
 
     connect_from_client, clientAddress = serverSocket.recvfrom(2048)
     print 'From client: '+ connect_from_client
@@ -26,23 +28,25 @@ while 1:
     accept_connection = 'accept'
     serverSocket.sendto(accept_connection, clientAddress)
 
-
-
-
+    # Get the URL from the client
 
     fileurl, clientAddress = serverSocket.recvfrom(2048)
-    #connectionSocket, addr = serverSocket.accept()
-    #fileurl = connectionSocket.recv(4000)
-    #Try to get the total time for the process
+   
+    #Try to get the total time for the process. Start:
     start = timer()
-
+    
+    # Get the file from the URL sent by the client. 
+    # In order to deal with exceptions we implement a try-except block
+    # we use exceptions from the "requests" module. 
+    # On the server we have the specific error - HTTP, Connection, Timeout
+    # or a generic error. A message is sent to the client.
+    
     try:
         r = requests.get(fileurl, allow_redirects=True)
         r.raise_for_status() 
     except requests.exceptions.HTTPError as err:
         print err
         serverSocket.sendto('Http Error', clientAddress)
-        #connectionSocket.send('Http error')
         sys.exit(1)
     except requests.exceptions.ConnectionError as errcon:
         print ("Error Connecting:",errcon)
@@ -53,57 +57,49 @@ while 1:
         serverSocket.sendto('Timeout Error', clientAddress)
         sys.exit(1)
     except:
-       print ("Generic Error")
-       serverSocket.sendto('Generic Error', clientAddress)
-       sys.exit(1)
+        print ("Generic Error")
+        serverSocket.sendto('Generic Error', clientAddress)
+        sys.exit(1)
     
     
-    # Check the file size
+    # Optional: check the file size from the headers
+    
     fileSize = r.headers['Content-length']
     
     print 'Client request new file. Size of the file: ', fileSize
 
       
     # Find the name of the file based on URL
+    
     if fileurl.find('/'):
         print 'Getting ' + fileurl.rsplit('/', 1)[1]
-       # fileToClient = fileurl.rsplit('/', 1)[1]
+    
         fileToClient = fileurl.rsplit('/', 1)[1]
     
-    # Create an empty file to store the content of the file
-    #######OKOK open(fileToClient, 'wb').write(r.content)
+    # Create an empty file to store the content of the file and open in write mode
+
     open(fileToClient, 'wb').write(r.content)
     
-    
-    
-    #First: send the size of the file from the header to the client to compare
+    #First: send the name of the file. We add "clientFile" to the name of the file
 
-    #connectionSocket.send(fileSize)
     message = "clientFile"+fileToClient
     serverSocket.sendto(message, clientAddress)
-    #serverSocket.sendto(fileToClient, clientAddress)
     
-    #Second send the file to client
+    #Second send the file to client. This is the main part
     
-    #connectionSocket.send(fileToClient)
     serverSocket.sendto(fileToClient, clientAddress)
     
-    f=open(fileToClient,"rb")
-    dataTosend = f.read(1024)
+    f=open(fileToClient,"rb") # Open the file in read mode
+    dataTosend = f.read(1024) # Read with 1024 bit buffer
     while (dataTosend):
         if(serverSocket.sendto(dataTosend,clientAddress)):
-            #print "sending ..."
             dataTosend = f.read(1024)
             time.sleep(0.02)# Give receiver a bit time to save
     f.close()
     
-    #Send the size:
-    
-    
     
     # Then wait for the BYE message from the client in order to close
     
-    #byeMessage = connectionSocket.recv(4000)
     byeMessage,clientAddress  = serverSocket.recvfrom(2048)
     print 'From client: ' , byeMessage
     if byeMessage == 'BYE':
@@ -114,7 +110,5 @@ while 1:
         totTime = (end - start)
         print 'Total time: ', totTime, 'sec.'
         print
-        #connectionSocket.send(str(timeTodw))
         serverSocket.sendto(str(totTime), clientAddress)
-        #serverSocket.close()
 
